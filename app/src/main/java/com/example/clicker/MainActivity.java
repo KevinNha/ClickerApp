8d7e9c508e22fd919f13528289ed6e054ec84b1b
Hyunjin Nha <hyunjinnha@gmail.com>
2020-12-16 20:09:59 -0800

    Quick simple working app

--- app/src/main/java/com/example/clicker/MainActivity.java
+++ app/src/main/java/com/example/clicker/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.clicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int count = 0;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView countTextView = (TextView) findViewById(R.id.textView);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                countTextView.setText(Integer.toString(count));
+            }
+        });
     }
 }
